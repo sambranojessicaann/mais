0149e6b65888e959d86355de4f623af5742ddc81
ikka1 <jessicaann.sambrano@gsfe.tupcavite.edu.ph>
2021-06-16 16:16:18 +0800

    finalizing templates

--- Laundrijess/views.py
+++ Laundrijess/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render, redirect
+import random
 
 def MainPage(request):
 
 	return render(request, 'mainpage.html')
+
+#remove parenthesis in ref number
+def create_new_ref_number():
+                not_unique = True
+                while not_unique:
+                    unique_ref = random.randint(1000000000, 9999999999)
+                    if not Transaction.objects.filter(Referrence_Number=unique_ref):
+                        not_unique = False
+                return str(unique_ref)
 
